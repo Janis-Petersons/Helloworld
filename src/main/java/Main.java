b824c80d2642304717ede7b33c55be284cbc0c63
Janis-Petersons <janis.petersons8@gmail.com>
2022-10-02 23:28:59 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Mani sauc Jaanis");
+        System.out.println("Es maacos");
+    };
+}
---
+++ src/main/java/lekcijaViens/labDarbs/Main.java
@@ -0,0 +1,48 @@
+package lekcijaViens.labDarbs;
+
+public class Main {
+    public static void main(String[] args) {
+//int- integer satur veselu skaitli
+        int vecums = 66;
+        System.out.println(vecums);
+        System.out.println( "Mans ir gadi:" +vecums);
+
+        int kurjuizmers = 43;
+                System.out.println("Kurpju izmērs ir:" +kurjuizmers);
+        String sunaVards = "Tobis";
+        String teksts= "Suņa vārds ir : ";
+        System.out.println(teksts+sunaVards);
+//maksimālā integer vērtība
+int maxInt = 1234567891;
+int temperature = -10;
+        System.out.println("Ziemā būs:" +temperature);
+// byte pieņem veselu skaitli no -127 līdz +127
+        byte vecumsDivi = 99;
+
+float apavuzimers = 44.5F;
+System.out.print("apavu izmers: " +apavuzimers);
+
+double grauduSvars = 150.3;
+System.out.println( "Tika pārdoti " +grauduSvars + "kg graudi");
+
+//vēlāk definējam vērtību
+int summa;
+summa = 5;
+        System.out.println(summa);
+
+//kalkulators zemāk - sarēķinās kopā 15
+        int a=5;
+        int b=10;
+        int ssumma;
+        ssumma = a + b ;
+        System.out.println(ssumma);
+
+int x=10;
+int y=-45;
+int rezultats = x+y;
+System.out.println(rezultats);
+
+
+
+    }
+}
