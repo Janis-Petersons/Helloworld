e529d74f71d1a5ad24ae3f32ca43a002f8707132
Janis-Petersons <janis.petersons8@gmail.com>
2022-10-03 21:19:28 +0300

    testsupdate

--- src/main/java/lekcijaViens/majasDarbs/Main.java
+++ src/main/java/lekcijaViens/majasDarbs/Main.java
@@ -1,4 +1,10 @@
 package lekcijaViens.majasDarbs;
 
 public class Main {
+    public static void main(String[] args) {
+        // majas darbs - valstis
+
+int koka = 36;
+System.out.print(koka);
+    }
 }
